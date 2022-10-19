e4883149ae3c01e521aa278b55dcc216fb7b0e8d
Noe Sola <ariadnnasc@gmail.com>
2022-10-19 17:23:54 +0200

    create db.json

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -3,11 +3,11 @@ import ApplicationForm from "../components/ApplicationForm";
 import Error from "../components/Error";
 
 // el request puede tardar en procesar los datos por eso la función es asíncrona.
-
 export async function action ({request}){
     const formData = await request.formData()
 // para poder acceder a los datos de formData     
     const datos = Object.fromEntries(formData)
+    const email = formData.get('email')
 
     //Validación
     const errors = []
@@ -15,6 +15,11 @@ export async function action ({request}){
         errors.push('Todos los datos son obligatorios')
     }
 
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+    if(!regex.test(email)){
+        errors.push('Email incorrecto')
+    }
     //Retornar datos si hay errores
     if(Object.keys(errors).length){
         return errors
@@ -36,7 +41,7 @@ const NewClient = () => {
 
         <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-15">
             {errors?.length && errors.map( (error, i)=> <Error key={i}>{error}</Error> )}
-            <Form method="POST" action>
+            <Form method="POST" noValidate>
                 <ApplicationForm/>
                 <input type="submit" className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg" value="Registrar cliente"/>
             </Form>
