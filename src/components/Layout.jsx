fcf3259484d12bbbf1961fe763948f9e75a1f6a7
Noe Sola <ariadnnasc@gmail.com>
2022-10-19 22:37:21 +0200

    fixed style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,14 +14,14 @@ const Layout = () => {
                 <nav className="mt-10">
                     <Link
                         className={`${location.pathname === "/" ? "text-blue-300 " : "text-white"
-                            }text-2xl block mt-2 hover:text-blue-300 text-white`}
+                            } text-2xl block mt-2 hover:text-blue-300 text-white`}
                         to="/"
                     >
                         Cliente
                     </Link>
                     <Link
                         className={`${location.pathname === "/" ? "text-blue-300 " : "text-white"
-                            }text-2xl block mt-2 hover:text-blue-300 text-white`}
+                            } text-2xl block mt-2 hover:text-blue-300 text-white`}
                         to="/clientes/nuevo"
                     >
                         Nuevo Cliente
