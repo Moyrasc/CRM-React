0f3eb77b470c90c25d04db5ec3e1eaff2dc1341e
Noe Sola <ariadnnasc@gmail.com>
2022-10-19 18:39:39 +0200

    feat: add method post

--- src/data/clients.js
+++ src/data/clients.js
@@ -6,5 +6,16 @@ export async function getClients () {
 }
 
 export async function postClient (datos) {
-
+    try {
+        const response = await fetch(import.meta.env.VITE_API_URL, {
+            method: 'POST',
+            body:JSON.stringify(datos),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        await response.json()
+    } catch (error) {
+        console.log(error)
+    }
 }
